911c7549090047bb94208679c8e6d1364f75dfa4
Puritechlab <pub@puritechlab.com>
2025-02-11 21:16:54 +0900

    edit

--- js/swiper.js
+++ js/swiper.js
@@ -9,19 +9,7 @@ var mvSwiper = new Swiper(".mv-swiper", {
     },
 });
 
-/* section04 */
-var s4swiper = new Swiper(".s4-swiper", {
-    autoplay: {
-        delay: 3000,
-    },
-    loop: true,
-    pagination: {
-        el: ".s4-swiper-pagination",
-        type: "fraction",
-    },
-});
-
-/* section01 (예약 페이지)*/
+/* section01 */
 var s1swiper = new Swiper(".s1-swiper", {
     navigation: {
         nextEl: ".swiper01-next",
@@ -49,3 +37,43 @@ var g1swiper = new Swiper(".g1-swiper", {
         prevEl: ".swiper01-pre",
     },
 });
+
+var tb1swiper = new Swiper(".tb1-swiper", {
+    allowTouchMove: false,
+});
+
+var tb2swiper = new Swiper(".tb2-swiper", {
+    allowTouchMove: true,
+    loop:false,
+    freeMode: true,
+    resistanceRatio: 0.85,
+    slidesPerView: 'auto',
+});
+
+var tb3swiper = new Swiper(".tb3-swiper", {
+    allowTouchMove: true,
+    loop:false,
+    freeMode: true,
+    resistanceRatio: 0.85,
+    slidesPerView: 'auto',
+});
+
+var tb4swiper = new Swiper(".tb4-swiper", {
+    allowTouchMove: true,
+    loop:false,
+    freeMode: true,
+    resistanceRatio: 0.85,
+    slidesPerView: 'auto',
+});
+
+// /* section04 */
+// var s4swiper = new Swiper(".s4-swiper", {
+//     autoplay: {
+//         delay: 3000,
+//     },
+//     loop: true,
+//     pagination: {
+//         el: ".s4-swiper-pagination",
+//         type: "fraction",
+//     },
+// });
