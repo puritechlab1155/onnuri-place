75cdff8332c34022e541c67b572cea347ab4e268
Puritechlab <pub@puritechlab.com>
2025-02-11 15:03:54 +0900

    edit

--- js/swiper.js
+++ js/swiper.js
@@ -21,14 +21,24 @@ var s4swiper = new Swiper(".s4-swiper", {
     },
 });
 
-/* section01 */
+/* section01 (예약 페이지)*/
 var s1swiper = new Swiper(".s1-swiper", {
-    pagination: {
-        el: ".s1-swiper-pagination",
-        type: "fraction",
-    },
     navigation: {
-        nextEl: ".mv-swiper-button-next",
-        prevEl: ".mv-swiper-button-prev",
+        nextEl: ".swiper01-next",
+        prevEl: ".swiper01-pre",
+    },
+});
+
+var s2swiper = new Swiper(".s2-swiper", {
+    navigation: {
+        nextEl: ".swiper02-next",
+        prevEl: ".swiper02-pre",
+    },
+});
+
+var s3swiper = new Swiper(".s3-swiper", {
+    navigation: {
+        nextEl: ".swiper03-next",
+        prevEl: ".swiper03-pre",
     },
 });
